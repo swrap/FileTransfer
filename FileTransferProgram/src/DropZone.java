e24679391b3f452d33ccbcc0763ceb68b4cafb6e
Jesse Saran <Jesse Saran@192.168.1.5>
2015-12-29 15:11:43 -0500

    Files are movable and can be enlarged Files can now be moved around in the area and can be increased in size by double clicking and decreased in size by double right clicking
    Next steps are:
    	-finish the files so the file name is visible
    		-fix the image for the files
    	-finish sending/receiving files
    	-work on modifying how files are sent/received
    		-find out more about max sizes for files and so on
    			-basically seeing how to speed it up or slow it down
    	-edit the gui to make it more beautiful
    -Swrap:

--- FileTransferProgram/src/DropZone.java
+++ FileTransferProgram/src/DropZone.java
@@ -11,16 +11,22 @@ import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileSystemView;
 
 public class DropZone extends JPanel
@@ -34,57 +40,143 @@ public class DropZone extends JPanel
         super();
         this.zone = new ZoneListener();
         this.target = new DropTarget(this,zone);
+        this.setLayout(null);
     }
     
     public void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
         Dimension dim = this.getSize();
         String s = "Drop Here";
         g.drawString(s, (int)(dim.getWidth()/2),(int)(dim.getHeight()/2));
-        for(DropObject d : drops)
-        {
-            System.out.println("HERE: " + (int)d.getX() + " " + (int)(d.getY()));
-            g.drawImage(d.getImage(), (int)(d.getX()), (int)(d.getY()), null);
-        }
+        //was manually drawing the images, not they are done in the component
+//        for(DropObject d : drops)
+//        {
+//            System.out.println("HERE: " + d.getX() + " " + d.getY());
+//            g.drawImage(d.getImage(), d.getX(), d.getY(), null);
+//        }
     }
     
-    private class DropObject
+    private class DropObject extends JComponent implements MouseMotionListener, MouseListener
     {
         private Image img;
         private String name = "";
-        private Point loc;
+        private Point loc, moved, clicked;
         
         public DropObject(File file, Point loc)
         {
+            super();
             Icon icon = FileSystemView.getFileSystemView().getSystemIcon(file);
             this.img = ((ImageIcon)icon).getImage();
             this.name = file.getName();
             this.loc = loc;
+            this.moved = new Point(0,0);
+            
+            int width = img.getWidth(null);
+            int height = img.getHeight(null);
+            
+            this.setMinimumSize(new Dimension(width, height));
+            this.setPreferredSize(new Dimension(width, height));
+            this.setBounds(loc.x, loc.y, width, height);
+            
+            this.setLocation(loc);
+            this.setVisible(true);
+            
+            this.addMouseMotionListener(this);
+            this.addMouseListener(this);
+            this.setFocusable(true);
+            
+            this.setToolTipText(name);
         }
         
-        public Image getImage()
+        public void paintComponent(Graphics g)
         {
-            return img;
+            g.drawImage(img, 0, 0, null);
+            g.drawString(name, 0, this.getHeight());
         }
-        
-        public String name()
+
+        @Override
+        public void mouseDragged(MouseEvent e)
         {
-            return name;
+            moved = e.getPoint();
         }
-        
-        public void setX(int x, int y)
-        {
-            loc = new Point(x,y);
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
         }
-        
-        public double getX()
+
+        @Override
+        public void mouseClicked(MouseEvent e)
         {
-            return loc.getX();
+            if(e.getClickCount() == 2 && SwingUtilities.isLeftMouseButton(e))
+            {
+                int width = (int)(this.getWidth()*1.2);
+                int height = (int)(this.getHeight()*1.2);
+                if(width < DropZone.this.getWidth() && height < DropZone.this.getHeight())
+                {
+                    img = img.getScaledInstance(width, height, Image.SCALE_FAST);
+                    this.setMinimumSize(new Dimension(width, height));
+                    this.setPreferredSize(new Dimension(width, height));
+                    this.setBounds(loc.x, loc.y, width, height);
+                }
+            }
+            else if(e.getClickCount() == 2 && SwingUtilities.isRightMouseButton(e))
+            {
+                int width = (int)(this.getWidth()/1.2);
+                int height = (int)(this.getHeight()/1.2);
+                if(width > 10 && height > 10)
+                {
+                    img = img.getScaledInstance(width, height, Image.SCALE_FAST);
+                    this.setMinimumSize(new Dimension(width, height));
+                    this.setPreferredSize(new Dimension(width, height));
+                    this.setBounds(loc.x, loc.y, width, height);
+                }
+            }
         }
-        
-        public double getY()
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e)
         {
-            return loc.getY();
+            clicked = e.getLocationOnScreen();
+            Point temp = this.getLocationOnScreen();
+            clicked.x = clicked.x < temp.x ? temp.x - clicked.x : clicked.x - temp.x;
+            clicked.y = clicked.y < temp.y ? temp.y - clicked.y : clicked.y - temp.y;
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e)
+        {
+            if(moved.x != 0 && moved.y != 0)
+            {
+                this.setLocation(loc.x + moved.x - clicked.x, loc.y + moved.y - clicked.y);
+                Point p = this.getLocation();
+                if(p.x < 0)
+                    p.setLocation(0,p.y);
+                else if(p.x + this.getWidth() > DropZone.this.getWidth())
+                    p.setLocation(DropZone.this.getWidth() - this.getWidth(), p.y);
+                if(p.y < 0)
+                    p.setLocation(p.x, 0);
+                else if(p.y + this.getHeight() > DropZone.this.getHeight())
+                    p.setLocation(p.x, DropZone.this.getHeight() - this.getHeight());
+                this.setLocation(p);
+                loc = p;
+                moved = new Point(0,0);
+            }
+            revalidate();
         }
     }
     
@@ -135,7 +227,9 @@ public class DropZone extends JPanel
                         List<File> files = (List<File>) trans.getTransferData(f);
                         for(File file : files)
                         {
-                            drops.add(new DropObject(file, e.getLocation()));
+                            DropObject dr = new DropObject(file, e.getLocation());
+                            drops.add(dr);
+                            DropZone.this.add(dr);
                         }
                     }
                 }
@@ -148,6 +242,8 @@ public class DropZone extends JPanel
             }
             
             e.dropComplete(true);
+            DropZone.this.revalidate();
+            DropZone.this.repaint();
         }
 
         @Override
